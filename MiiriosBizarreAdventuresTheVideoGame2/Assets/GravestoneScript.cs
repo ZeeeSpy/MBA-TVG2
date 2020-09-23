f2b09b08ed4913f60554e64e5432d205dc7a935f
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-09-23 13:34:34 +0100

    SR Graveyard
    Replaced protien powder with correct brand. Started work on SR graveyard

---
+++ MiiriosBizarreAdventuresTheVideoGame2/Assets/GravestoneScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravestoneScript : MonoBehaviour, Interactable
+{
+	private bool inter = false;
+	public string InteractWithObject()
+	{
+		if (!inter)
+		{
+			PayRespectsScript.instance.Checkin();
+			inter = true;
+		}
+		return "";
+	}
+}
---
+++ MiiriosBizarreAdventuresTheVideoGame2/Assets/PayRespectsScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PayRespectsScript : MonoBehaviour
+{
+	public static PayRespectsScript instance;
+	private bool firstsetup = true;
+	private int count = 0;
+
+	private Text PRText;
+
+	void Start()
+	{
+		if (instance != this && instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+		}
+
+		if (firstsetup)
+		{
+			firstsetup = false;
+
+			PRText = GetComponent<Text>();
+		}
+	}
+
+
+	public void Checkin()
+	{
+		count++;
+		PRText.text = $"Pay Respects: {count}/10";
+	}
+}
