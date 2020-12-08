96d687597b06761d0d0538a003169bf22a3963c9
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-12-08 13:06:04 +0000

    MaldKing ending

---
+++ MiiriosBizarreAdventuresTheVideoGame2/Assets/LevelSwitch2D.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch2D : MonoBehaviour
+{
+	public string nextscene;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			SceneManager.LoadScene(nextscene);
+		}
+	}
+}
