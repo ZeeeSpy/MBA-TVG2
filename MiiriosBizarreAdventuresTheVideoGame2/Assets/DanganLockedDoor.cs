ab8ff0cf5dddb8acc522c3a6aa84647daa8e751e
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-12-08 15:44:25 +0000

    Minimapstuff, Locked Doors in DGM and Text fix in myrow house

---
+++ MiiriosBizarreAdventuresTheVideoGame2/Assets/DanganLockedDoor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DanganLockedDoor : MonoBehaviour, Interactable
+{
+	private AudioSource AS;
+
+	void Start()
+	{
+		AS = GetComponent<AudioSource>();
+		AS.clip = AudioClipMaster.instance.GetClip(1);
+	}
+
+	public string InteractWithObject()
+	{
+		AS.Play();
+		return null;
+	}
+}
---
+++ MiiriosBizarreAdventuresTheVideoGame2/Assets/interactTextResetOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class interactTextResetOnCollision : MonoBehaviour
+{
+	public Text UIText;
+
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			UIText.text = "";
+		}
+	}
+
+}
