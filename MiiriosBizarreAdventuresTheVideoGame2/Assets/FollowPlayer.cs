31b598eac7f630ef84061c9058b2d817c4b401c4
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-12-09 15:38:51 +0000

    Minimap parent rotation fix

---
+++ MiiriosBizarreAdventuresTheVideoGame2/Assets/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+	public Transform Player;
+
+    void Update()
+    {
+		transform.position = new Vector3(Player.position.x, transform.position.y, Player.position.z);
+    }
+}
